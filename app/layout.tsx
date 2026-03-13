82de8e311fd37a2bc7e0ca63fb3e39401dc29099
j-s-leee <q216044@gmail.com>
2026-03-13 15:10:57 +0900

    Fix manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/logo/film.svg", type: "image/svg+xml" },
       { url: "/logo/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/logo/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
